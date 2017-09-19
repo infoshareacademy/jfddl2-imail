ffa4ca809868564abe652506bed45872f5f0e7b6
Daniel Jaremek <djaremek11@gmail.com>
2017-09-19 14:51:00 +0200

    cookies&photos

--- js/script.js
+++ js/script.js
@@ -59,3 +59,18 @@ $(document).ready(function() {
         });
     }
 });
+
+$().ready(function() {
+    var sName = "cookiesok";
+    $("#close-cookie-warn").click(function(){
+        var oExpire = new Date();
+        oExpire.setTime((new Date()).getTime() + 2000);
+        document.cookie = sName + "=1;expires=" + oExpire;
+        $("#cookie-warn").hide("slow");
+    });
+    var sStr = '; '+ document.cookie +';';
+    var nIndex = sStr.indexOf('; '+ escape(sName) +'=');
+    if (nIndex === -1) {
+        $("#cookie-warn").show();
+    }
+});
